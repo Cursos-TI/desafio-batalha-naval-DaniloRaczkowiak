35d9a30b637f64f77e8011a6271424cea3c42e72
DaniloRaczkowiak <contato@dlautomacoes.com.br>
2025-11-11 04:48:01 -0300

    Update batalhaNaval.c
    incluindo #define e mais 2 navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
+#define TAMANHO 10 //definido o tamanho do tabuleiro
 int main (){
 
-    int tabuleiro[10][10];
+    int tabuleiro[TAMANHO][TAMANHO];
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -17,6 +19,16 @@ int main (){
     tabuleiro[7][3] =3;
     tabuleiro[7][4] =3;
     tabuleiro[7][5] =3;
+
+    //navio 3 coordenadas:
+    tabuleiro[0][7] = 3;
+    tabuleiro[1][8] = 3;
+    tabuleiro[2][9] = 3;
+
+    //navio 4 coordenadas:
+    tabuleiro[5][6] = 3;
+    tabuleiro[6][7] = 3;
+    tabuleiro[7][8] = 3;
      printf("Visualização do Tabuleiro (10x10):\n");
     printf("    A   B   C   D   E   F   G   H   I   J \n"); // Cabeçalho de Colunas
    
